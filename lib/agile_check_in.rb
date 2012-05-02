143d7acca6f99b4c11251e017dfd45affcaa4ffc
phillc <spyyderz@gmail.com>
2012-05-02 13:04:03 -0400

    Moving our check in logic to a gem

--- lib/agile_check_in.rb
+++ lib/agile_check_in.rb
@@ -1,5 +1,56 @@
 require "agile_check_in/version"
 
 module AgileCheckIn
-  # Your code goes here...
+  def incremental
+    pair_names = ""
+    story_number = ""
+
+    history_file = '.shove_history.yml'
+    if File.exists?(history_file)
+      shove_history = YAML::load(File.open(history_file))["shove"]
+      pair_names    = shove_history["pair"]
+      story_number  = shove_history["story"]
+    end
+
+    begin
+      $stdout.write "Pair names (separated with '/') [#{pair_names}]: "
+      input = $stdin.gets.strip
+      pair_names = input unless input.blank?
+    end until !pair_names.blank?
+
+    begin
+      $stdout.write "Story number [#{story_number}]: "
+      input = $stdin.gets.strip
+      story_number = input unless input.blank?
+    end until !story_number.blank?
+
+    File.open(history_file, 'w') do |out|
+      YAML.dump({ "shove" => { "pair" => pair_names, "story" => story_number } }, out)
+    end
+
+    commit_message = "[#{pair_names} - ##{story_number}] "
+
+    system("git add -A")
+    system("EDITOR=vim git commit -e -m '#{commit_message}'")
+  end
+
+  def push_and_test
+    puts "*******"
+    puts "About to test these changes:"
+    puts `git log origin/develop..HEAD`
+    puts "*******"
+
+
+    if system("rake spec")
+      puts "*******"
+      puts "About to push these changes:"
+      puts `git log origin/develop..HEAD`
+      puts "*******"
+      puts "Shoving..."
+      system("git push origin develop")
+    else
+      puts "Tests failed. Shove aborted."
+      exit(1)
+    end
+  end
 end
